a9bddf155f5d1a5d63e80eb3636ed792c2b04a0a
User-2024VFCXJC\Administrator <rkaehd127@gmail.com>
2025-02-12 15:36:22 +0900

    apitest.py 에서  xml 데이터 파싱 함수 api.py 로 수정

--- RealEstateAnalysis/api.py
+++ RealEstateAnalysis/api.py
@@ -1,5 +1,5 @@
 import requests
-import datetime
+import xml.etree.ElementTree as ET
 def get_data(gu_code, base_date):
     url = 'http://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade?'
     service_key = 'iX7cWRfHPxT7QQh9jUylodoYKug1nxysQgUp%2BsQIffV%2B1IkgnrcgM2tdIFN51daq%2BDbQV1Aqp0l6Hx6mnLqc9g%3D%3D'
@@ -9,3 +9,16 @@ def get_data(gu_code, base_date):
 
     res = requests.get(url + payload)
     return res
+def get_items(response):
+    root = ET.fromstring(response.content)
+    item_list = []
+    for child in root.find('body').find('items'):
+        elements = child.findall('*')
+        data = {}
+        for element in elements:
+            tag = element.tag.strip()
+            text = element.text.strip()
+            # print tag, text
+            data[tag] = text
+        item_list.append(data)
+    return item_list
